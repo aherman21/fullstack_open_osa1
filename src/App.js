5a24aed8119e1280d19dd04586d6269a9b1bf2c5
aherman21 <hermansalexander2@gmail.com>
2023-05-22 14:49:43 +0300

    tehtävä 1.3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const Header = (props) => {
 
 
 const Total = (props) => {
+  console.log(props)
   return (
     <div>
       <p>Number of exercises {props.total}</p>
@@ -16,17 +17,19 @@ const Total = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}></Part>
-      <Part part={props.part2} exercises={props.exercises2}></Part>
-      <Part part={props.part3} exercises={props.exercises3}></Part>
+      <Part part={props.part1.name} exercises={props.part1.excercises}></Part>
+      <Part part={props.part3.name} exercises={props.part2.excercises}></Part>
+      <Part part={props.part2.name} exercises={props.part3.excercises}></Part>
     </div>
   )
 }
 
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
       <p>{props.part} {props.exercises}</p>
@@ -36,24 +39,30 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    excercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    excercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    excercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content part1={part1}
+      <Content
+       part1={part1}
        part2 = {part2}
        part3 = {part3}
-       exercises1 = {exercises1}
-       exercises2 = {exercises2}
-       exercises3 = {exercises3}>
+       >
       </Content>
-      <Total total={exercises1+exercises2+exercises3}></Total>
+      <Total total={part1.excercises+part2.excercises+part3.excercises}></Total>
     </div>
   )
 }
