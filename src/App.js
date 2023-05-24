68ebd2eb37510568635319316a69c16e9c89dd90
aherman21 <hermansalexander2@gmail.com>
2023-05-24 13:21:33 +0300

    Tehtävä 1.4 korjattu

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.props[0].exercises+props.props[1].exercises+props.props[2].exercises}</p>
     </div>
   )
 }
@@ -20,9 +20,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.part1.name} exercises={props.part1.excercises}></Part>
-      <Part part={props.part3.name} exercises={props.part2.excercises}></Part>
-      <Part part={props.part2.name} exercises={props.part3.excercises}></Part>
+      <Part props={props.props[0]}></Part>
+      <Part props={props.props[1]}></Part>
+      <Part props={props.props[2]}></Part>
     </div>
   )
 }
@@ -32,7 +32,7 @@ const Part = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <p>{props.props.name} {props.props.exercises}</p>
     </div>
   )
 }
@@ -40,30 +40,19 @@ const Part = (props) => {
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
-  {
-    name: 'Fundamentals of React',
-    excercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    excercises: 7
-  },
-  {
-    name: 'State of a component',
-    excercises: 14
-  }
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
   ]
 
   return (
     <div>
       <Header course={course}></Header>
       <Content
-       part1={parts[0]}
-       part2 = {parts[1]}
-       part3 = {parts[2]}
+       props={parts}
        >
       </Content>
-      <Total total={parts[0].excercises+parts[1].excercises+parts[2].excercises}></Total>
+      <Total props={parts}></Total>
     </div>
   )
 }
